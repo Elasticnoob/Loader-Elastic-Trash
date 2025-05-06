0b5680eba98e70003b74153bd89675f29f497f92
uwontallowedtofindinme <85240878+Elasticnoob@users.noreply.github.com>
2025-05-07 00:30:14 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,8 @@
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8e86d9c4dc56a21db8c715465c953fba.lua"))()
 
+wait(2)
+
 --bypass anti cheat
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Elasticnoob/adonis/refs/heads/main/bypass%20ad.lua'))()
 
